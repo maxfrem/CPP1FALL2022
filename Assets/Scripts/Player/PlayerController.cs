0924c55dd0db1d004ea93e46b9650b1489c6264a
maxfrem <maxime.frenette@torontofilmschool.ca>
2022-10-25 02:35:02 -0400

    lab 3 update (still incomplete)
    Figured out previous issue just need to add attack and jump attack for sub

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,22 +5,73 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
+    //components etc.
     Rigidbody2D rb;
     Animator anim;
     SpriteRenderer sr;
+
+    //mouvement stuff
+    public float speed;
+    public float jumpForce;
+
+    //Attack Stuff
+    
+    
+    //grounc checking
+    public bool isGrounded;
+    public Transform GroundCheck;
+    public LayerMask isgGroundLayer;
+    public float GroundCheckRadius;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+
+        if (speed <= 0)
+        {
+            speed = 6.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+        }
+
+        if (!GroundCheck)
+        {
+            GroundCheck = GameObject.FindGameObjectWithTag("Groundcheck").transform;
+        }
+
+        if (GroundCheckRadius <= 0)
+        {
+            GroundCheckRadius = 0.2f;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
+        float hInput = Input.GetAxisRaw("Horizontal");
 
-        Debug.Log(hInput);
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, isgGroundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection; 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
